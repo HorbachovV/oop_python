1ae81ca2e9f317b3368e49017a47de3aa21865a4
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2023-03-01 21:31:16 +0200

    oop principles

--- oop_python.py
+++ oop_python.py
@@ -1,7 +1,30 @@
+# oop principles:
+# encapsulation
+# abstraction
+# inheritance
+# polyphormism
+
 from item import Item
 from phone import Phone
 
-item1 = Item('MyItem', 750)
-item1.name = 'OtherNameaaaaaaa'
+# item1 = Item('MyItem', 750, 6)
 
-print(item1.name)
+# item1.name = 'OtherNameaaaaaaa'
+# item1.apply_increment(0.2)
+# item1.apply_discount()
+
+# print(item1.price)
+
+
+# item2 = Phone('Xiaomi', 9999, 3)
+# item2.send_email()
+
+
+name = 'Vova'
+print(len(name))
+
+some_list = ['some', 'list']
+
+print(len(some_list))
+
+#That's polyphormism in action , a single function does know how to handle different kinds of objects as expected
